8d146f903b61435f186190435eab13a58a512b21
Phlxrd <ph993983@gmail.com>
2025-04-10 19:49:08 -0300

    Atualiza settings.py para ambiente Render e configurações de produção

--- create_superuser.py
+++ create_superuser.py
@@ -1,9 +1,12 @@
-# create_superuser.py
-
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
+username = "admin"
+email = "admin@admin.com"
+password = "admin123"
 
-if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('admin', 'admin@example.com', 'admin123')
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username=username, email=email, password=password)
     print("Superusuário criado com sucesso!")
+else:
+    print("Superusuário já existe.")
